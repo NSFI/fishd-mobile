d1948f474a55828ec0e0428546477281b006713e
lizhaoxuan <lizhaoxuan@corp.netease.com>
2020-01-16 18:57:54 +0800

    沟通后重新初始化

--- source/components/Button/index.tsx
+++
@@ -1,9 +0,0 @@
-import Button from './Button';
-import ButtonGroup from './ButtonGroup';
-import './style/index.less';
-
-export { ButtonProps, ButtonShape, ButtonSize, ButtonType } from './Button';
-export { ButtonGroupProps } from './ButtonGroup';
-
-Button.Group = ButtonGroup;
-export default Button;
--- source/components/index.js
+++
@@ -1,4 +0,0 @@
-// 方便控制打包时该资源放在最顶部
-import '../assets/css/index.less';
-
-export {default as Button} from './Button/index.tsx';
