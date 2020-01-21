8e55942b9636712b53c195299872f0136837d297
李兆轩 <lizhaoxuan@corp.netease.com>
2020-01-21 10:31:55 +0800

    add：增加demo补充，增加fishd-doc依赖

--- source/components/Button/Button.tsx
+++ source/components/Button/Button.tsx
@@ -1,11 +1,45 @@
 import * as React from 'react';
-import { findDOMNode } from 'react-dom';
 import * as PropTypes from 'prop-types';
 
-class Button extends React.Component {
+import './style/index.less'
+
+type ButtonType = 'primary' | 'default' | 'dashed' | 'danger';
+type ButtonSize = 'small' | 'large' | 'default';
+
+export interface ButtonProps {
+  type?:ButtonType,
+  size?:ButtonSize,
+}
+
+class Button extends React.Component<any> {
+  static defaultProps = {
+    prefixCls: 'fishd-btn',
+    type: 'default',
+    size: 'default'
+  };
+  static propTypes = {
+    type: PropTypes.string,
+    size: PropTypes.string,
+  };
+  constructor(props) {
+    super(props);
+  }
   render() {
+    const { type, size, ...otherProps} = this.props;
+    let sizeCls = '';
+    switch (size) {
+      case 'large':
+        sizeCls = 'lg';
+        break;
+      case 'small':
+        sizeCls = 'sm';
+        break;
+      default:
+        break;
+    }
+    const classForBtn = `fishd-btn fishd-btn-${type} ${sizeCls && `fishd-btn-${sizeCls}`}`;
     return (
-      <button>demo按钮</button>
+      <button type={type} className={classForBtn} {...otherProps}>{this.props.children}</button>
     );
   }
 }
