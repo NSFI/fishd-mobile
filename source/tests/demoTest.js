8e7f056666cb117f4e417bd3a7133bbc850c1979
hangaoke1 <188869009@qq.com>
2020-05-14 16:57:28 +0800

    feat: 优化单元测试

--- source/tests/demoTest.js
+++ source/tests/demoTest.js
@@ -1,16 +1,35 @@
-import glob from 'glob';
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable global-require */
+
 import React from 'react';
 import { render } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
-export default function webDemoTest(component, options = {}) {
-  const files = glob.sync(`./source/components/${component}/demo/*.js`);
-  files.forEach(file => {
-    const testMethod = options.skip === true ? test.skip : test;
-    testMethod(`renders ${file} correctly`, () => {
-      const Demo = require(`../.${file}`).default; // eslint-disable-line global-require, import/no-dynamic-require
-      const wrapper = render(<Demo />);
-      expect(toJson(wrapper)).toMatchSnapshot();
-    });
+const getDemoContainer = demoName => {
+  if (!demoName) return null;
+  const demoContainerClass = require(`../../site/docs/zh-CN/${demoName}.md`);
+
+  if (typeof demoContainerClass === 'function') {
+    return demoContainerClass;
+  }
+  if (
+    demoContainerClass &&
+    typeof demoContainerClass === 'object' &&
+    typeof demoContainerClass.default === 'function'
+  ) {
+    return demoContainerClass.default;
+  }
+  return null;
+};
+
+export default function webDemoTest(compName, options = {}) {
+  const demoName = compName.substr(0, 1).toLowerCase() + compName.substr(1);
+  const testMethod = options.skip === true ? test.skip : test;
+
+  testMethod(`Renders ${compName} demo correctly`, () => {
+    const Container = getDemoContainer(demoName);
+    expect(Container).not.toBeNull();
+    const wrapper = render(<Container />); // eslint-disable-line global-require, import/no-dynamic-require
+    expect(toJson(wrapper)).toMatchSnapshot();
   });
 }
