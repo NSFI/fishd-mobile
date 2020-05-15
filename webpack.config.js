83228ad8dd7c25a6d1f7faafe7bf7467ae5a76af
zrj1031 <zrj961031@163.com>
2020-05-15 14:25:06 +0800

    chore: remove some eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,42 @@
-// TODO
-module.exports = {};
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    'fishd-mobile': './source/components/index.js',
+  },
+  output: {
+    library: 'fishd-mobile',
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].min.js',
+    chunkFilename: '[id].min.js',
+  },
+  mode: 'production',
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+      {
+        test: /\.less$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].min.css',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
