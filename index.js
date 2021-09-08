c1fc18406e859ce47d6f446128d7cc4a5c835e2f
hangaoke <hangaoke@corp.netease.com>
2021-09-08 13:56:25 +0800

    ci: 🎡 修复demo更新后，注册事件无法清楚问题

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// const req = require.context('./source/components', true, /^\.\/[^_][\w-]+\/style\/index\.less?$/);
-// req.keys().forEach(mod => {
-//   req(mod);
-// });
 const req = require.context('./source/components', true, /^\.\/[^_][\w-]+\/style\/index?$/);
 req.keys().forEach(mod => {
   req(mod);
